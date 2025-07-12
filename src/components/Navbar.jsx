fa79173e60c41400c5b8dc3c100d0bbdc8a095df
Nisha Shaw <nishashaw@Nishas-MacBook-Air.local>
2025-07-12 16:15:58 +0530

    implemented hide menubar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setMenu] = useState(false);
   const [token, setToken] = useState(true);
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)  
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img onClick={() => navigate("/")} src={assets.logo} alt="" className="w-44 cursor-pointer" />
@@ -54,17 +54,20 @@ const Navbar = () => {
         )}
         <img className="w-6 md:hidden" onClick={() => setMenu(true)} src={assets.menu_icon} alt="" />
         {/* ------------- Mobile  Menu -------------*/}
-        <div className={`${showMenu ? "fixed w-full": "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+        <div className={`${showMenu? "fixed w-full": "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
           <div className="flex items-center justify-between px-5 py-6">
             <img className="w-36" src={assets.logo} alt="" />
             <img className="w-7 h-7 rounded-full hover:scale-155  transition-all duration-500" onClick={() => setMenu(false)} src={assets.cross_icon} alt="" />
           </div>
-          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          {token? <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
             <NavLink className="w-full"  to='/' onClick={() => setMenu(false)}><p className="px-4 py-2  h-14 rounded w-full flex items-center justify-center hover:bg-indigo-200 text-xl hover:text-2xl">HOME</p></NavLink>
             <NavLink className="w-full" to='/doctors' onClick={() => setMenu(false)}><p className="px-4 py-2 h-14 rounded w-full flex items-center justify-center hover:bg-indigo-200 text-xl hover:text-2xl">ALL DOCTORS</p></NavLink>
             <NavLink className="w-full" to='/about' onClick={() => setMenu(false)}><p className="px-4 py-2 h-14 rounded w-full flex items-center justify-center hover:bg-indigo-200 text-xl hover:text-2xl">ABOUT</p></NavLink>
             <NavLink className="w-full" to='/contact' onClick={() => setMenu(false)}><p className="px-4 py-2 h-14 rounded w-full flex items-center justify-center hover:bg-indigo-200 text-xl hover:text-2xl">CONTACT</p></NavLink>
+          </ul>: <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink className="w-full"  to='/login' onClick={() => setMenu(false)}><p className="px-4 py-2  h-14 rounded w-full flex items-center justify-center hover:bg-indigo-200 text-xl hover:text-2xl">LOGIN</p></NavLink>
           </ul>
+            }
         </div>
       </div>
     </div>
